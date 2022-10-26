31f4e5f45a3091566f0618a03493bb54dce34b17
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-26 16:42:48 +0200

    ellenorzesek kellenek

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 "use strict";
-let userNamePattern = /^[a-z]{3,}$/;
-console.log(userNamePattern.test("bob"));
-console.log(userNamePattern.test("!@1"));
-console.log(userNamePattern.test("z"));
-let emailPattern = /[a-z0-9.-]{1,}@[a-z.]{1,}/g;
-let szoveg = 'A petrik email cime: petrik@petrik.hu, a titkárásgé: titkarsag@petrik.hu';
+/*
 let emailCimek = szoveg.matchAll(emailPattern);
 for (let email of emailCimek) {
     console.log(email[0]);
-}
+} */
 document.addEventListener('DOMContentLoaded', () => {
+    var _a;
+    // Patternek:
+    let elsoHaromPattern = /[a-zA-Z ]{1,}/;
+    let utcaHazPattern = / [a-zA-Z0-9_ ]{1,}/;
+    let irszPattern = / A-Z0-9{1,}/;
+    let bankKartyaSzamPattern = / ^[0-9]{1,}-[0-9]{1,}-[0-9]{1,}-[0-9]{1,}$/;
+    let kodPattern = / ^[0-9]{3}$/;
+    let tulajdonosPattern = /[a-zA-Z ]/;
+    (_a = document.getElementById('fizetes')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+        // étrékek:
+        let nev = document.getElementById('nev').value;
+        let orszag = document.getElementById('orszag').value;
+        let varos = document.getElementById('varos').value;
+        let utca = document.getElementById('utcaHaz').value;
+        let irsz = document.getElementById('irsz').value;
+        let bankKartyaSzam = document.getElementById('kartyaSzam').value;
+        let kod = document.getElementById('kod').value;
+        let tulajdonos = document.getElementById('tulajdonos').value;
+        // Ellenorzesek
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
-
-
-let userNamePattern = /^[a-z]{3,}$/;
-console.log(userNamePattern.test("bob"));
-console.log(userNamePattern.test("!@1"));
-console.log(userNamePattern.test("z"));
-
-let emailPattern = /[a-z0-9.-]{1,}@[a-z.]{1,}/g;
-let szoveg = 'A petrik email cime: petrik@petrik.hu, a titkárásgé: titkarsag@petrik.hu'
-
+/*
 let emailCimek = szoveg.matchAll(emailPattern);
 for (let email of emailCimek) {
     console.log(email[0]);
-}
+} */
 
 document.addEventListener('DOMContentLoaded',() => {
-    
+    // Patternek:
+    let elsoHaromPattern = /[a-zA-Z ]{1,}/;
+    let utcaHazPattern = / [a-zA-Z0-9_ ]{1,}/; 
+    let irszPattern = / A-Z0-9{1,}/;    
+    let bankKartyaSzamPattern = / ^[0-9]{1,}-[0-9]{1,}-[0-9]{1,}-[0-9]{1,}$/;
+    let kodPattern = / ^[0-9]{3}$/;
+    let tulajdonosPattern = /[a-zA-Z ]/;
+
+    document.getElementById('fizetes')?.addEventListener('click', () => {
+        // étrékek:
+        let nev = (document.getElementById('nev') as HTMLInputElement).value;
+        let orszag = (document.getElementById('orszag') as HTMLInputElement).value;
+        let varos = (document.getElementById('varos') as HTMLInputElement).value;
+        let utca = (document.getElementById('utcaHaz') as HTMLInputElement).value;
+        let irsz = (document.getElementById('irsz') as HTMLInputElement).value;
+        let bankKartyaSzam = (document.getElementById('kartyaSzam') as HTMLInputElement).value;
+        let kod = (document.getElementById('kod') as HTMLInputElement).value;
+        let tulajdonos = (document.getElementById('tulajdonos') as HTMLInputElement).value;
+        // Ellenorzesek
+        
+            
+        
+
+    });
 });
 
