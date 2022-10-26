342d8f12a7cba8a088888079aa5087b42dc4d926
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-26 17:28:48 +0200

    Irsz nem jó

--- src/index.js
+++ src/index.js
@@ -9,20 +9,66 @@ document.addEventListener('DOMContentLoaded', () => {
     // Patternek:
     let elsoHaromPattern = /[a-zA-Z ]{1,}/;
     let utcaHazPattern = / [a-zA-Z0-9_ ]{1,}/;
-    let irszPattern = / A-Z0-9{1,}/;
+    let irszPattern = / ^[A-Z][0-9]{1,}$/;
     let bankKartyaSzamPattern = / ^[0-9]{1,}-[0-9]{1,}-[0-9]{1,}-[0-9]{1,}$/;
     let kodPattern = / ^[0-9]{3}$/;
     let tulajdonosPattern = /[a-zA-Z ]/;
     (_a = document.getElementById('fizetes')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+        // Minden hibát kiszed
+        document.getElementById('rosszNev').textContent = '';
+        document.getElementById('rosszOrszag').textContent = '';
+        document.getElementById('rosszVaros').textContent = '';
+        document.getElementById('rosszUtca').textContent = '';
+        document.getElementById('rosszIrsz').textContent = '';
+        document.getElementById('rosszKartya').textContent = '';
+        document.getElementById('rosszKod').textContent = '';
+        document.getElementById('rosszTulajdonos').textContent = '';
         // étrékek:
         let nev = document.getElementById('nev').value;
         let orszag = document.getElementById('orszag').value;
         let varos = document.getElementById('varos').value;
         let utca = document.getElementById('utcaHaz').value;
         let irsz = document.getElementById('irsz').value;
-        let bankKartyaSzam = document.getElementById('kartyaSzam').value;
+        let bankKartyaSzam = document.getElementById('kartyaSzam').value.toUpperCase();
         let kod = document.getElementById('kod').value;
         let tulajdonos = document.getElementById('tulajdonos').value;
         // Ellenorzesek
+        let joNev = elsoHaromPattern.test(nev);
+        let joOrzsag = elsoHaromPattern.test(orszag);
+        let joVaros = elsoHaromPattern.test(varos);
+        let joUtca = utcaHazPattern.test(utca);
+        let joIrsz = irszPattern.test(irsz);
+        let joKartya = bankKartyaSzamPattern.test(bankKartyaSzam);
+        let joKod = kodPattern.test(kod);
+        let joTulajdonos = tulajdonosPattern.test(tulajdonos);
+        // Hibák:
+        if (!joNev) {
+            document.getElementById('rosszNev').textContent = 'Ez a név nem felel meg a közetelményeknek!';
+        }
+        else if (!joOrzsag) {
+            document.getElementById('rosszOrszag').textContent = 'Ez az ország nem felel meg a közetelményeknek!';
+        }
+        else if (!joVaros) {
+            document.getElementById('rosszVaros').textContent = 'Ez a város nem felel meg a közetelményeknek!';
+        }
+        else if (!joUtca) {
+            document.getElementById('rosszUtca').textContent = 'Ez az utca nem felel meg a közetelményeknek!';
+        }
+        else if (!joIrsz) {
+            document.getElementById('rosszIrsz').textContent = 'Ez az írányítószám nem felel meg a közetelményeknek!';
+        }
+        else if (!joKartya) {
+            document.getElementById('rosszKartya').textContent = 'Ez a bank kártya nem felel meg a közetelményeknek!';
+        }
+        else if (!joKod) {
+            document.getElementById('rosszKod').textContent = 'Ez a CCV nem felel meg a közetelményeknek!';
+        }
+        else if (!joTulajdonos) {
+            document.getElementById('rosszTulajdonos').textContent = 'Ez a tulajdonos nem felel meg a közetelményeknek!';
+        }
+        else {
+            document.getElementById('form').innerHTML = '';
+            document.getElementById('form').innerHTML = '<p class="siker"> Sikeres fizetés! </p>';
+        }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -8,25 +8,60 @@ document.addEventListener('DOMContentLoaded',() => {
     // Patternek:
     let elsoHaromPattern = /[a-zA-Z ]{1,}/;
     let utcaHazPattern = / [a-zA-Z0-9_ ]{1,}/; 
-    let irszPattern = / A-Z0-9{1,}/;    
+    let irszPattern = / ^[A-Z][0-9]{1,}$/;    
     let bankKartyaSzamPattern = / ^[0-9]{1,}-[0-9]{1,}-[0-9]{1,}-[0-9]{1,}$/;
     let kodPattern = / ^[0-9]{3}$/;
     let tulajdonosPattern = /[a-zA-Z ]/;
 
     document.getElementById('fizetes')?.addEventListener('click', () => {
+        // Minden hibát kiszed
+        (document.getElementById('rosszNev') as HTMLElement).textContent = '';
+        (document.getElementById('rosszOrszag') as HTMLElement).textContent = '';
+        (document.getElementById('rosszVaros') as HTMLElement).textContent = '';
+        (document.getElementById('rosszUtca') as HTMLElement).textContent = '';
+        (document.getElementById('rosszIrsz') as HTMLElement).textContent = '';
+        (document.getElementById('rosszKartya') as HTMLElement).textContent = '';
+        (document.getElementById('rosszKod') as HTMLElement).textContent = '';
+        (document.getElementById('rosszTulajdonos') as HTMLElement).textContent = '';
         // étrékek:
         let nev = (document.getElementById('nev') as HTMLInputElement).value;
         let orszag = (document.getElementById('orszag') as HTMLInputElement).value;
         let varos = (document.getElementById('varos') as HTMLInputElement).value;
         let utca = (document.getElementById('utcaHaz') as HTMLInputElement).value;
         let irsz = (document.getElementById('irsz') as HTMLInputElement).value;
-        let bankKartyaSzam = (document.getElementById('kartyaSzam') as HTMLInputElement).value;
+        let bankKartyaSzam = (document.getElementById('kartyaSzam') as HTMLInputElement).value.toUpperCase();
         let kod = (document.getElementById('kod') as HTMLInputElement).value;
         let tulajdonos = (document.getElementById('tulajdonos') as HTMLInputElement).value;
         // Ellenorzesek
-        
-            
-        
+        let joNev = elsoHaromPattern.test(nev);
+        let joOrzsag = elsoHaromPattern.test(orszag);
+        let joVaros = elsoHaromPattern.test(varos);
+        let joUtca = utcaHazPattern.test(utca);
+        let joIrsz = irszPattern.test(irsz);
+        let joKartya = bankKartyaSzamPattern.test(bankKartyaSzam);
+        let joKod = kodPattern.test(kod);
+        let joTulajdonos = tulajdonosPattern.test(tulajdonos);
+        // Hibák:
+        if (!joNev){
+            (document.getElementById('rosszNev') as HTMLElement).textContent = 'Ez a név nem felel meg a közetelményeknek!';
+        }else if(!joOrzsag){
+            (document.getElementById('rosszOrszag') as HTMLElement).textContent = 'Ez az ország nem felel meg a közetelményeknek!';
+        }else if (!joVaros) {
+            (document.getElementById('rosszVaros') as HTMLElement).textContent = 'Ez a város nem felel meg a közetelményeknek!';
+        }else if(!joUtca){
+            (document.getElementById('rosszUtca') as HTMLElement).textContent = 'Ez az utca nem felel meg a közetelményeknek!';
+        }else if(!joIrsz){
+            (document.getElementById('rosszIrsz') as HTMLElement).textContent = 'Ez az írányítószám nem felel meg a közetelményeknek!';
+        }else if(!joKartya){
+            (document.getElementById('rosszKartya') as HTMLElement).textContent = 'Ez a bank kártya nem felel meg a közetelményeknek!';
+        }else if (!joKod) {
+            (document.getElementById('rosszKod') as HTMLElement).textContent = 'Ez a CCV nem felel meg a közetelményeknek!';
+        }else if (!joTulajdonos) {
+            (document.getElementById('rosszTulajdonos') as HTMLElement).textContent = 'Ez a tulajdonos nem felel meg a közetelményeknek!';
+        }else {
+            (document.getElementById('form')as HTMLElement).innerHTML = '';
+            (document.getElementById('form') as HTMLElement).innerHTML = '<p class="siker"> Sikeres fizetés! </p>'
+        }
 
     });
 });
